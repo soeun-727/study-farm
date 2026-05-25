3726827d2a40a1e1343aba1ad21060e470873d05
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-05-25 22:38:43 +0900

    chore: App.tsx 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
-
-import Home from "./pages/Home";
-import MyPage from "./pages/MyPage";
-
 export default function App() {
-  const location = useLocation();
-
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={location.pathname}
-        className="h-screen w-screen"
-        initial={{ x: "100%" }}
-        animate={{ x: 0 }}
-        exit={{ x: "-100%" }}
-        transition={{ duration: 0.4, ease: "easeInOut" }}
-      >
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </motion.div>
-    </AnimatePresence>
+    <>
+      <h1>App Component</h1>
+    </>
   );
 }
