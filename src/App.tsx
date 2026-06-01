c1b8d6aaaf55d132ba3b47eaa48879ed7b527db5
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-06-02 01:58:39 +0900

    feat: 수정모드 완성 및 확인 모달 연결 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import StudyRecordModal from "./components/Modal/StudyRecordModal";
-
 export default function App() {
-  return (
-    <>
-      <StudyRecordModal
-        month={5}
-        day={15}
-        time="10:00"
-        studyTime="2시간"
-        studyContent="React 컴포넌트 설계"
-        memo={["importante note"]}
-      />
-    </>
-  );
+  return <></>;
 }
