870ba58da606db86406e8594a0efa839675f547e
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-06-03 23:32:00 +0900

    feat: 프로필 이미지 매핑 로직 추가

--- src/components/myStudyFarm/Profile.tsx
+++ src/components/myStudyFarm/Profile.tsx
@@ -1,17 +1,30 @@
-import { lvl2farmer } from "../../assets";
-import { riceBackground } from "../../assets/cropBackground/cropBackgroundIndex";
 import MyCrops from "./myCrops";
 import MyStats from "./myStats";
 
 import { mockMidFarmer } from "../../constants/userStatData";
+import {
+  CROP_BACKGROUNDS,
+  FARMER_COMPONENTS,
+} from "../../constants/profileImage";
 
 export default function Profile() {
+  //임시 데이터
+  const { currentCrop, currentLevel } = mockMidFarmer;
+
+  const selectedBackground =
+    CROP_BACKGROUNDS[currentCrop] || CROP_BACKGROUNDS["쌀"];
+  const FarmerComponent =
+    FARMER_COMPONENTS[currentLevel] || FARMER_COMPONENTS[1];
+
   return (
     <div className="flex w-230 h-85 px-5 justify-between">
       {/* 이미지 영역 */}
       <div className="flex">
-        <img src={riceBackground} className="self-start w-75 rounded-[10px]" />
-        <img src={lvl2farmer} className="self-end w-45 -ml-25 z-10" />
+        <img
+          src={selectedBackground}
+          className="self-start w-75 rounded-[10px]"
+        />
+        <img src={FarmerComponent} className="self-end w-45 -ml-25 z-10" />
       </div>
       {/* 정보 영역 */}
       <div className="flex flex-col justify-between pb-1">
