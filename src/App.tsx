375049ef25051c15cfdc7eecdd73fa28fa0f5af4
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-06-16 21:03:57 +0900

    test: 테스트용 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages 임포트
-import HomePage from './pages/HomePage';
-import MyCalendarPage from './pages/MyCalendarPage';
-import MyStudyFarmPage from './pages/MyStudyFarmPage';
-import RankPage from './pages/RankPage';
-import RankingDetailPage from './pages/RankingDetailPage';
+import HomePage from "./pages/HomePage";
+import MyCalendarPage from "./pages/MyCalendarPage";
+import MyStudyFarmPage from "./pages/MyStudyFarmPage";
+import RankPage from "./pages/RankPage";
+import RankingDetailPage from "./pages/RankingDetailPage";
 
 // Auth 관련 페이지 임포트 (추후 구현 예정)
 // import InitialPage from './pages/InitialPage';
@@ -13,33 +13,33 @@ import RankingDetailPage from './pages/RankingDetailPage';
 
 export default function App() {
   return (
-    <Router>
-   
-        <Routes>
-          {/* 인증 및 초기 진입 경로 지정 전*/}
-          {/* <Route path="/" element={<InitialPage />} /> */}
-          {/* <Route path="/login" element={<LoginPage />} /> */}
+    <HomePage />
+    // <Router>
 
-            {/* 홈 (타이머 등 메인) */}
-            <Route path="/home" element={<HomePage />} />
+    //     <Routes>
+    //       {/* 인증 및 초기 진입 경로 지정 전*/}
+    //       {/* <Route path="/" element={<InitialPage />} /> */}
+    //       {/* <Route path="/login" element={<LoginPage />} /> */}
 
-            {/* 마이 홈 */}
-            <Route path="/mystudyfarm" element={<MyStudyFarmPage />} />
-            
-            {/* 스터디 캘린더 */}
-            <Route path="/calendar" element={<MyCalendarPage />} />
-            
-            {/* 랭킹 */}
-            <Route path="/rank">
-              <Route index element={<RankPage />} />
-              <Route path="detail/:id" element={<RankingDetailPage />} />
-            </Route>
-       
+    //         {/* 홈 (타이머 등 메인) */}
+    //         <Route path="/home" element={<HomePage />} />
 
-          {/* 3. 기타 독립 페이지 */}
-          {/* <Route path="/settings" element={<SettingsPage />} /> */}
-          
-        </Routes>
-    </Router>
+    //         {/* 마이 홈 */}
+    //         <Route path="/mystudyfarm" element={<MyStudyFarmPage />} />
+
+    //         {/* 스터디 캘린더 */}
+    //         <Route path="/calendar" element={<MyCalendarPage />} />
+
+    //         {/* 랭킹 */}
+    //         <Route path="/rank">
+    //           <Route index element={<RankPage />} />
+    //           <Route path="detail/:id" element={<RankingDetailPage />} />
+    //         </Route>
+
+    //       {/* 3. 기타 독립 페이지 */}
+    //       {/* <Route path="/settings" element={<SettingsPage />} /> */}
+
+    //     </Routes>
+    // </Router>
   );
 }
