e6512169e0902e9e31b203643526bff8ef373d9b
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-06-18 00:12:29 +0900

    fix: import문 누락 문제 해결

--- src/components/auth/Welcom.tsx
+++ src/components/auth/Welcom.tsx
@@ -1,14 +1,21 @@
-import Button from '../ui/Button';
+import Button from "../ui/Button";
+
+// 1. 사용할 이미지들을 상단에서 import 합니다.
+import lv1farmerTop from "../../assets/characters/lv1farmer_ver4_top.svg";
+import appleCrop from "../../assets/crops/apple.svg";
+import mangoCrop from "../../assets/crops/mango.svg";
+import starFruitCrop from "../../assets/crops/starFruit.svg";
+import blueberryCrop from "../../assets/crops/blueberry.svg";
+import watermelonCrop from "../../assets/crops/watermelon.svg";
 
 interface WelcomProps {
   onGoToLogin: () => void;
   onGoToSignUp: () => void;
 }
 
-export default function Welcom({onGoToLogin, onGoToSignUp }: WelcomProps) {
+export default function Welcom({ onGoToLogin, onGoToSignUp }: WelcomProps) {
   return (
     <div className="flex w-full min-h-screen">
-        
       {/* 1초 뒤 농부 주변으로 과일 360도 회전하는 애니메이션 정의 */}
       <style>{`
         @keyframes revolveGroup {
@@ -44,105 +51,93 @@ export default function Welcom({onGoToLogin, onGoToSignUp }: WelcomProps) {
 
       `}</style>
 
-
-     
-
       {/* 중앙 */}
       <main className="flex-1 bg-(--primary-light-brown)">
         <div className="min-h-screen flex flex-col items-center justify-center p-8">
-          
           {/* 온보딩 이미지 구현 */}
           <div className="farm-scene-container relative w-[480px] h-[400px]">
-            
             {/* 농부 */}
-            <img 
-              src="./src/assets/characters/lv1farmer_ver4_top.svg" 
+            <img
+              src={lv1farmerTop}
               alt="손 흔드는 농부"
               className="absolute w-[200px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20"
             />
 
             {/* 과일들 배치 */}
             <div className="absolute inset-0 z-10 animate-revolve-group">
-            {/* 좌측 끝: 사과 */}
-            <div className="absolute top-[45%] left-[10%] -translate-y-1/2 -rotate-30 ">
-              <img 
-                src="./src/assets/crops/apple.svg" 
-                alt="사과"
-                className="w-[60px] fruit-fade-5"
-              />
-            </div>
+              {/* 좌측 끝: 사과 */}
+              <div className="absolute top-[45%] left-[10%] -translate-y-1/2 -rotate-30 ">
+                <img
+                  src={appleCrop}
+                  alt="사과"
+                  className="w-[60px] fruit-fade-5"
+                />
+              </div>
 
-            {/* 좌측 상단: 망고 */}
-            <div className="absolute top-[23%] left-[24%] z-10 -rotate-12">
-              <img 
-                src="./src/assets/crops/mango.svg" 
-                alt="망고"
-                className="w-[70px] fruit-fade-4"
-              />
-            </div>
+              {/* 좌측 상단: 망고 */}
+              <div className="absolute top-[23%] left-[24%] z-10 -rotate-12">
+                <img
+                  src={mangoCrop}
+                  alt="망고"
+                  className="w-[70px] fruit-fade-4"
+                />
+              </div>
 
-             {/* 중앙: 스타후르츠 */}
-            <div className="absolute top-[15%] left-[45%] z-10">
-              <img 
-                src="./src/assets/crops/starFruit.svg" 
-                alt="스타후르츠"
-                className="w-[90px]  fruit-fade-3"
-              />
-            </div>
+              {/* 중앙: 스타후르츠 */}
+              <div className="absolute top-[15%] left-[45%] z-10">
+                <img
+                  src={starFruitCrop}
+                  alt="스타후르츠"
+                  className="w-[90px]  fruit-fade-3"
+                />
+              </div>
 
-            {/* 우측 상단: 블루베리 */}
-            <div className="absolute top-[20%] right-[20%] z-10 rotate-12">
-              <img 
-                src="./src/assets/crops/blueberry.svg" 
-                alt="블루베리"
-                className="w-[70px]  fruit-fade-2"
-              />
-            </div>
+              {/* 우측 상단: 블루베리 */}
+              <div className="absolute top-[20%] right-[20%] z-10 rotate-12">
+                <img
+                  src={blueberryCrop}
+                  alt="블루베리"
+                  className="w-[70px]  fruit-fade-2"
+                />
+              </div>
 
-            {/* 우측 끝: 수박 */}
-            <div className="absolute top-[45%] right-[5%] -translate-y-1/2 z-10 rotate-30">
-              <img 
-                src="./src/assets/crops/watermelon.svg" 
-                alt="수박"
-                className="w-[70px] fruit-fade-1"
-              />
-            </div>
+              {/* 우측 끝: 수박 */}
+              <div className="absolute top-[45%] right-[5%] -translate-y-1/2 z-10 rotate-30">
+                <img
+                  src={watermelonCrop}
+                  alt="수박"
+                  className="w-[70px] fruit-fade-1"
+                />
+              </div>
             </div>
           </div>
-        
 
-        {/*버튼 & 하단 텍스트 */}
-        <div className="w-[480px] flex flex-col items-center mt-6 z-20">
-          {/* 시작하기 버튼 */}
-        <Button 
-            onClick={onGoToLogin}
-            size="L"
-            variant="black"
-            className="w-[480px] !mx-auto"
-          >
+          {/*버튼 & 하단 텍스트 */}
+          <div className="w-[480px] flex flex-col items-center mt-6 z-20">
+            {/* 시작하기 버튼 */}
+            <Button
+              onClick={onGoToLogin}
+              size="L"
+              variant="black"
+              className="w-[480px] !mx-auto"
+            >
               시작하기
-          </Button>
+            </Button>
 
-        {/* 하단 회원가입 안내 텍스트 */}
-        <div className="flex items-center gap-3 mt-4 typo-label">
-            <span className="text-(--gray-600)">
-            아직 계정이 없나요?
-            </span>
-            {/* 회원가입 버튼: 마우스 올리면 밑줄 생기게 처리 */}
-            <button 
-            onClick={onGoToSignUp}
-            className="text-(--gray-800) typo-button hover:text-(--gray-900) hover:underline transition-colors">
-            회원가입
-            </button>
+            {/* 하단 회원가입 안내 텍스트 */}
+            <div className="flex items-center gap-3 mt-4 typo-label">
+              <span className="text-(--gray-600)">아직 계정이 없나요?</span>
+              {/* 회원가입 버튼: 마우스 올리면 밑줄 생기게 처리 */}
+              <button
+                onClick={onGoToSignUp}
+                className="text-(--gray-800) typo-button hover:text-(--gray-900) hover:underline transition-colors"
+              >
+                회원가입
+              </button>
+            </div>
+          </div>
         </div>
-        </div>
-        
-        </div>
-
-
       </main>
-
-      
     </div>
   );
 }
