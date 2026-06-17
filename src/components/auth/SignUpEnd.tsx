0c6ce50b2c56054ba67ece94c86f40ac472fff53
soeun-727 <soeun2215562@sookmyung.ac.kr>
2026-06-18 00:39:42 +0900

    fix: import문 오류 해결

--- src/components/auth/SignUpEnd.tsx
+++ src/components/auth/SignUpEnd.tsx
@@ -1,47 +1,36 @@
-import Button from '../ui/Button';
+import Button from "../ui/Button";
+import lv1farmerVer3Top from "../assets/characters/lv1farmer_ver3_top.svg";
 
 interface SignUpEndProps {
   onStart: () => void;
 }
 
 export default function SignUpEnd({ onStart }: SignUpEndProps) {
-return (
+  return (
     <div className="flex w-full min-h-screen">
-        <main className="flex-1  flex flex-col  items-center justify-center bg-(--primary-light-brown)">
-         <div className="p-8">
+      <main className="flex-1 flex flex-col items-center justify-center bg-(--primary-light-brown)">
+        <div className="p-8">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="typo-h1 py-6">회원가입 완료!</h2>
+            <div className="typo-body pb-6">열공농장에 오신 것을 환영해요</div>
 
-            <div className='flex flex-col items-center justify-center'>
-            <h2 className="typo-h1 py-6"> 
-                회원가입 완료!
-            </h2>
-            <div className="typo-body pb-6">
-                열공농장에 오신 것을 환영해요
-            </div>
-          
+            <img
+              src={lv1farmerVer3Top}
+              alt="만세하는 농부"
+              className="w-[200px] z-20 mb-0"
+            />
 
-         {/* 농부 */}
-         <img 
-            src="./src/assets/characters/lv1farmer_ver3_top.svg" 
-            alt="만세하는 농부"
-            className="w-[200px] z-20 mb-0" 
-          />
-           
-     
-       <Button 
-          onClick={onStart}
-          size="L"
-          variant="black"
-          className="!w-[400px] mt-4"
-        >
-            시작하기
-        </Button>
+            <Button
+              onClick={onStart}
+              size="L"
+              variant="black"
+              className="!w-[400px] mt-4"
+            >
+              시작하기
+            </Button>
+          </div>
         </div>
-        </div>
-    
-
-
-        </main>
+      </main>
     </div>
-
-)
+  );
 }
